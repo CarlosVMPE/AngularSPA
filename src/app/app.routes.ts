abe48dd2079a165f3aeb7e358b1a944546caaae8
Carlos <1310374@utp.edu.pe>
2019-05-01 00:37:49 -0500

    useHash base href

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
